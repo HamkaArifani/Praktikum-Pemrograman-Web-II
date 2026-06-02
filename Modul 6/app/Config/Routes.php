6b0f0c0667aa5713bdae7b41d40aa05cb5a35bce
HamkaArifani <hamka0897@gmail.com>
2026-06-02 22:58:47 +0800

    Fix: sync deletion of file outside prak601 to GitHub

--- Modul 6/app/Config/Routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use CodeIgniter\Router\RouteCollection;
-
-/** @var RouteCollection $routes */
-$routes->get('/', 'PagesController::Home');
-$routes->get('profil', 'PagesController::Profile');
