2d8f76f3378b84976cb8b1eac443259395843580
HamkaArifani <hamka0897@gmail.com>
2026-06-13 20:16:11 +0800

    feat: completing buku and user migration files

--- Modul 7/PRAK701/app/Database/Migrations/2026-06-12-150404_Buku.php
+++ Modul 7/PRAK701/app/Database/Migrations/2026-06-12-150404_Buku.php
@@ -8,11 +8,37 @@ class Buku extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'BIGINT',
+                'constraint' => 12,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'judul' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'penulis' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'penerbit' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'tahun_terbit' => [
+                'type' => 'YEAR',
+                'constraint' => '4',
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('Buku');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('Buku');
     }
 }
--- Modul 7/PRAK701/app/Database/Migrations/2026-06-12-150430_User.php
+++ Modul 7/PRAK701/app/Database/Migrations/2026-06-12-150430_User.php
@@ -8,11 +8,33 @@ class User extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id'=> [
+                'type'=> 'INT',
+                'constraint' => '12',
+                'unsigned' => 'true',
+                'auto_increment'=> 'true',
+            ],
+            'username'=> [
+                'type' => 'VARCHAR',
+                'constraint' => '50',
+            ],
+            'email'=> [
+                'type' => 'VARCHAR',
+                'constraint' => '50',
+            ],
+            'password'=> [
+                'type' => 'TEXT',
+                'constraint' => '50',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('user');
+
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('user');
     }
 }
