f1bb847afa230837c7d1046651b5195d67f776c9
HamkaArifani <hamka0897@gmail.com>
2026-06-13 21:40:04 +0800

    feat: minor modify for UserModel.php

--- Modul 7/PRAK701/app/Models/UserModel.php
+++ Modul 7/PRAK701/app/Models/UserModel.php
@@ -17,17 +17,20 @@ class UserModel extends Model {
     protected array $casts = [];
     protected array $castHandlers = [];
 
+    // Dates
     protected $useTimestamps = false;
     protected $dateFormat = 'datetime';
     protected $createdField = 'created_at';
     protected $updatedField = 'updated_at';
     protected $deletedField = 'deleted_at';
 
+    // Validation
     protected $validationRules = [];
     protected $validationMessages = [];
     protected $skipValidation = false;
     protected $cleanValidationRules = true;
 
+    // Callbacks
     protected $allowCallbacks = true;
     protected $beforeInsert = ['hashPassword'];
     protected $afterInsert = [];
