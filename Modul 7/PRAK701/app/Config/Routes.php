a61b0946851f2136cbe9efd10afb3a5de9d1f034
HamkaArifani <hamka0897@gmail.com>
2026-06-14 23:24:39 +0800

    feat: add routes for authentication and buku

--- Modul 7/PRAK701/app/Config/Routes.php
+++ Modul 7/PRAK701/app/Config/Routes.php
@@ -3,4 +3,20 @@
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
-$routes->get('/', 'Home::index');
+
+$routes->get('/', 'BukuController::index');
+$routes->get('buku', 'BukuController::index');
+
+$routes->get('login', 'AuthController::index');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+$routes->post('register', 'AuthController::register');
+
+$routes->group('buku', ['filter' => 'auth'], function($routes) {
+    $routes->get('tabel', 'BukuController::tabel');
+    $routes->get('create', 'BukuController::create');
+    $routes->post('store', 'BukuController::store');
+    $routes->get('edit/(:num)', 'BukuController::edit/$1');
+    $routes->post('update/(:num)', 'BukuController::update/$1');
+    $routes->get('delete/(:num)', 'BukuController::delete/$1');
+});
